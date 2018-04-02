ed67f633035091a7a3edc00d3ae69f6fb25157cf
Otavio Santana <otavio.santana@walmart.com>
2018-04-01 22:34:42 -0300

    Add spring boot main applicattion

---
+++ src/main/java/com/moip/Application.java
@@ -0,0 +1,12 @@
+package com.moip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
